b71768af2c5ab27fe09cb712a77316337e76eba6
EugeneTan9 <tyx.eugene@gmail.com>
2019-11-21 09:31:26 +0800

    Did the MVP

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -1,15 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import tweets from 'tweets.js';
 
 class App extends React.Component {
   render() {
+    console.log(tweets);
+    let alltweets = tweets.map(tweets=>{
+        return (
+            <div>
+                <p>{tweets.created_at}</p>
+                <p>{tweets.id}</p>
+                <p>{tweets.text}</p>
+                <p>Likes: {tweets.favorite_count}</p>
+            </div>
+        );
+    })
     return (
       <div>
-        tweets
+        <h1>Tweets</h1>
+        {alltweets}
       </div>
     );
   }
 }
+
+
 
 const element = document.getElementById('app');
 
